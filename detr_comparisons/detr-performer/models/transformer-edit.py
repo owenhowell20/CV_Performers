4e721f91d8972f2b8a6f26ccf8a621fada36e66c
JGittles <jordandgittleman@gmail.com>
2022-12-15 15:39:57 -0500

    final commit

--- detr_comparisons/detr-performer/models/transformer-edit.py
+++ detr_comparisons/detr-performer/models/transformer-edit.py
@@ -1,17 +1,24 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """
 DETR Transformer class.
+
 Copy-paste from torch.nn.Transformer with modifications:
     * positional encodings are passed in MHattention
     * extra LN at the end of encoder is removed
     * decoder returns a stack of activations from all decoding layers
+
+
+    added in performer attention modules where applicable --- Jordan
 """
 import copy
 from typing import Optional, List
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn, Tensor
+from performer_pytorch import FastAttention, SelfAttention
+from .slimformer import MultiHeadAttention
 
 
 class Transformer(nn.Module):
@@ -20,6 +27,9 @@ class Transformer(nn.Module):
                  num_decoder_layers=6, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False,
                  return_intermediate_dec=False):
+        # nb_features = int(dim_heads*np.log(dim_heads))
+        # self.self_attn = FastAttention(dim_heads=dim_heads, nb_features=nb_features)
+        # self.multihead_attn = Fa
         super().__init__()
 
         encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward,
@@ -128,7 +138,11 @@ class TransformerEncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False):
         super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.self_attn = MultiHeadAttention(hidden_dim=d_model, n_heads = nhead, feature_type="favor+_528",compute_type="ps")
+        # dim_heads = d_model//nhead
+        # nb_features = int(dim_heads*np.log(dim_heads))
+        # self.self_attn = FastAttention(dim_heads=dim_heads, nb_features=nb_features)
+
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.dropout = nn.Dropout(dropout)
@@ -151,8 +165,9 @@ class TransformerEncoderLayer(nn.Module):
                      src_key_padding_mask: Optional[Tensor] = None,
                      pos: Optional[Tensor] = None):
         q = k = self.with_pos_embed(src, pos)
-        src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
+        # src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
+        #                      key_padding_mask=src_key_padding_mask)[0]
+        src2 = self.self_attn(src,pos,queries=q,keys=k,values=src)
         src = src + self.dropout1(src2)
         src = self.norm1(src)
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
@@ -166,8 +181,9 @@ class TransformerEncoderLayer(nn.Module):
                     pos: Optional[Tensor] = None):
         src2 = self.norm1(src)
         q = k = self.with_pos_embed(src2, pos)
-        src2 = self.self_attn(q, k, value=src2, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
+        # src2 = self.self_attn(q, k, value=src2, attn_mask=src_mask,
+        #                      key_padding_mask=src_key_padding_mask)[0]
+        src2 = self.self_attn(src2, pos,queries=q,keys=k,values=src2)
         src = src + self.dropout1(src2)
         src2 = self.norm2(src)
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src2))))
@@ -188,8 +204,12 @@ class TransformerDecoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False):
         super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        self.multihead_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.self_attn = MultiHeadAttention(hidden_dim=d_model, n_heads = nhead, feature_type="favor+_528",compute_type="ps")
+        self.multihead_attn = MultiHeadAttention(hidden_dim=d_model, n_heads = nhead, feature_type="favor+_528",compute_type="ps")
+        # dim_heads = d_model//nhead
+        # nb_features = int(dim_heads*np.log(dim_heads))
+        # self.self_attn = FastAttention(dim_heads=dim_heads, nb_features=nb_features)
+        # self.multihead_attn = FastAttention(dim_heads=dim_heads, nb_features=nb_features)
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.dropout = nn.Dropout(dropout)
@@ -216,14 +236,16 @@ class TransformerDecoderLayer(nn.Module):
                      pos: Optional[Tensor] = None,
                      query_pos: Optional[Tensor] = None):
         q = k = self.with_pos_embed(tgt, query_pos)
-        tgt2 = self.self_attn(q, k, value=tgt, attn_mask=tgt_mask,
-                              key_padding_mask=tgt_key_padding_mask)[0]
+        # tgt2 = self.self_attn(q, k, value=tgt, attn_mask=tgt_mask,
+        #                      key_padding_mask=tgt_key_padding_mask)[0]
+        tgt2 = self.self_attn(tgt,query_pos,queries=q,keys=k,values=tgt)
         tgt = tgt + self.dropout1(tgt2)
         tgt = self.norm1(tgt)
-        tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt, query_pos),
-                                   key=self.with_pos_embed(memory, pos),
-                                   value=memory, attn_mask=memory_mask,
-                                   key_padding_mask=memory_key_padding_mask)[0]
+        # tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt, query_pos),
+        #                           key=self.with_pos_embed(memory, pos),
+        #                           value=memory, attn_mask=memory_mask,
+        #                           key_padding_mask=memory_key_padding_mask)[0]
+        tgt2 = self.multihead_attn(tgt,query_pos,queries=q,keys=k,values=tgt)
         tgt = tgt + self.dropout2(tgt2)
         tgt = self.norm2(tgt)
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
@@ -240,14 +262,16 @@ class TransformerDecoderLayer(nn.Module):
                     query_pos: Optional[Tensor] = None):
         tgt2 = self.norm1(tgt)
         q = k = self.with_pos_embed(tgt2, query_pos)
-        tgt2 = self.self_attn(q, k, value=tgt2, attn_mask=tgt_mask,
-                              key_padding_mask=tgt_key_padding_mask)[0]
+        # tgt2 = self.self_attn(q, k, value=tgt2, attn_mask=tgt_mask,
+        #                      key_padding_mask=tgt_key_padding_mask)[0]
+        tgt2 = self.self_attn(tgt2, query_pos,queries=q,keys=k,values=tgt2)
         tgt = tgt + self.dropout1(tgt2)
         tgt2 = self.norm2(tgt)
-        tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt2, query_pos),
-                                   key=self.with_pos_embed(memory, pos),
-                                   value=memory, attn_mask=memory_mask,
-                                   key_padding_mask=memory_key_padding_mask)[0]
+        # tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt2, query_pos),
+        #                           key=self.with_pos_embed(memory, pos),
+        #                           value=memory, attn_mask=memory_mask,
+        #                           key_padding_mask=memory_key_padding_mask)[0]
+        tgt2 = self.multihead_attn(tgt2, query_pos,queries=q,keys=k,values=tgt2)
         tgt = tgt + self.dropout2(tgt2)
         tgt2 = self.norm3(tgt)
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt2))))
